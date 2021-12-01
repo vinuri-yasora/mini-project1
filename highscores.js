a98b6c4af20e3177a477fb88a257173405d0e7a3
vinuri-yasora <yasorarathnayake@gmail.com>
2021-12-02 00:34:03 +0530

    first commit

---
+++ highscores.js
@@ -0,0 +1,6 @@
+const highScoresList =document.querySelector('#highScoreList')
+const highScores = JSON.parse(localStorage.getItem('highScore')) || []
+
+highScoresList.innerHTML = highScores.map(score => {
+    return `<li class="high-score">${score.name} - ${score.score}</li>`
+}).join('')
